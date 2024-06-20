98fde626390e2bd312d37fe5df3bd2e758acf488
Majid-1273 <majidkhankhichi2004@gmail.com>
2024-06-21 04:01:15 +0500

    first commit

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -13,13 +13,13 @@ const ProjectCardData=[
     imgsrc: pro2,
     title: "Simon Game",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    view: "https://www.google.com/"
+    view: "https://simongame-js-jquery.netlify.app/"
   },
   {
     imgsrc: pro3,
     title: "Portfolio Website",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    view: "https://www.youtube.com"
+    view: "https://majidkhan.netlify.app/"
   }
 ]
 
