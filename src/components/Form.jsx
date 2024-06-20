b237c77640394708b938415c1405520d5a763812
Majid-1273 <majidkhankhichi2004@gmail.com>
2024-06-21 04:54:20 +0500

    first commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,13 +3,13 @@ import '../styles/Form.css'
 
 const Form = () => {
   return (
-      <form name="contact" className='form' netlify>
+      <form name="contact" method='POST' className='form' netlify>
       <label htmlFor="">Your Name</label>
       <input type="text" name="name" />
       <label htmlFor="">Email</label>
       <input type="email" name="email" />
       <label htmlFor="">Message</label>
-      <textarea name="" id="" rows={6} placeholder='Type Your Message Here'></textarea>
+      <textarea id="" rows={6} placeholder='Type Your Message Here' name="message"></textarea>
       <button className='btn' type='submit'>Submit</button>
       </form>
   )
