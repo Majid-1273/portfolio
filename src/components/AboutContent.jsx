7ddb9516a043ebc8e81a97be36cd7ae79ddbabf5
Majid-1273 <majidkhankhichi2004@gmail.com>
2024-06-21 03:18:06 +0500

    commit-msg

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -5,12 +5,23 @@ import img1 from '../assets/about1.jpg'
 import img2 from '../assets/about2.jpg'
 
 const AboutContent = () => {
+    const handleDownload = () => {
+    const fileUrl = '/resume.pdf';
+    
+    // Create a temporary anchor element to trigger the download
+    const link = document.createElement('a');
+    link.href = fileUrl;
+    link.download = 'resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
     return (
         <div className='about'>
             <div className="left">
                 <h1>Who Am I?</h1>
                 <p>I'm a React Front-End Developer. I create responsive secure websites for my clients.</p>
-                <Link to="/contact"><button className='btn btn-about'>Contact</button></Link>
+                <button className='btn btn-about' onClick={handleDownload}>Download Resume</button>
             </div>
             <div className="right">
                 <div className="img-container">
