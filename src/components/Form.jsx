248e9f745c6038f286a477852e11f5607a0a4248
Majid-1273 <majidkhankhichi2004@gmail.com>
2024-06-22 19:18:24 +0500

    first commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,18 +3,13 @@ import '../styles/Form.css'
 
 const Form = () => {
   return (
-    <form name="contact" className='form' data-netlify="true" netlify-honeypot="bot-field" hidden>
-      <input type="hidden" name="form-name" value="contact" />
-
-      <label htmlFor="name">Your Name</label>
-      <input type="text" id="name" name="name" />
-
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" name="email" />
-
-      <label htmlFor="message">Message</label>
-      <textarea id="message" rows={6} placeholder='Type Your Message Here' name="message"></textarea>
-
+    <form className='form'>
+      <label>Your Name</label>
+      <input type="text" />
+      <label>Email</label>
+      <input type="email" />
+      <label>Message</label>
+      <textarea id="message" rows={6} placeholder='Type Your Message Here'></textarea>
       <button className='btn' type='submit'>Submit</button>
     </form>
   )
